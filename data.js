f6ff20f0d40bc3d99d8542e147d0054f9c2c6984
konarsada <sadabratak@gmail.com>
2022-05-11 00:18:49 +0530

    constructor function, Object.assign(this, data), import-export

---
+++ data.js
@@ -0,0 +1,14 @@
+export default {
+    hero: {
+        name: "Wizard",
+        avatar: "images/wizard.png",
+        health: 60,
+        diceCount: 3
+    },
+    monster: {
+        name: "Orc",
+        avatar: "images/orc.png",
+        health: 10,
+        diceCount: 1
+    }
+}
